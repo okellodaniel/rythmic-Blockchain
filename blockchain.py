b620c12f9d53f19e54fc9568ac065f12196a0bce
Daniel Okello <handsdani@gmail.com>
2022-04-05 15:22:17 +0300

    added proof of work, todigest, block creation demantics

--- blockchain.py
+++ blockchain.py
@@ -5,9 +5,29 @@ import json as _json
 class Blockchain:
     def __init__(self) -> None:
         self.chain = list()
-        genesis_block = self.create_genesis_block(data='I am the genesis block', proof=1, prevoius_hash='0', index=1)
+        genesis_block = self._create_block(data='I am the genesis block', proof=1, prevoius_hash='0', index=1)
+
+        self.chain.append(genesis_block)
+
+    def mine_block(self,data:str) -> dict:
+        previous_block = self.get_previous_block()
+        previous_proof = previous_block['proof']
+        proof = None
+        index = len(self.chain) + 1
     
-    def create_genesis_block(self, data: str, proof: int, prevoius_hash: str, index:int) -> dict:
+
+    
+    def _proof_pf_work(self,previos_proof: int,data: str, index:int) -> int:
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+
+    def _to_digest(self,)        
+
+    def get_previous_block(self)-> dict:
+        return self.chain[-1]
+    
+    def _create_block(self, data: str, proof: int, prevoius_hash: str, index:int) -> dict:
         block = {
                 'index': index,
                 'timestamp': str(_dt.datetime.now()),
